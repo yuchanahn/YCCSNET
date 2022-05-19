29f727fcfa9bc163b39a9730026147f9b45fe617
IN-205-YuChanAhn <IN-205@DESKTOP-N0Q5JUV>
2022-05-19 15:03:57 +0900

    start

--- YCCSNET/Program.cs
+++ YCCSNET/Program.cs
@@ -1,9 +1,50 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using YCCSNET;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace YCCSNET {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+class Program {
+    public class user_t {
+        public int hash;
+        public IPEndPoint ip;
+    };
+
+    static void Main(string[] args) {
+        Dictionary<int, user_t> users = new Dictionary<int, user_t>();
+        
+
+        UdpClient udp = new UdpClient(9100);
+        try {
+            //DBManager.Init("localhost");
+            Console.WriteLine(" * UDP 서버가 시작되었습니다");
+        } catch (SocketException se) {
+            Console.WriteLine(se.ErrorCode + ": " + se.Message);
+            Environment.Exit(se.ErrorCode);
+        }
+        for (;;) {
+            try {
+                IPEndPoint r_ip = new IPEndPoint(IPAddress.Any, 0);
+                byte[] byteBuffer = udp.Receive(ref r_ip);
+                int id = r_ip.GetHashCode();
+
+                if (byteBuffer.Length == 1) continue;
+                if (!users.ContainsKey(id)) {
+                    Console.WriteLine("player code : " + id);
+                    Console.WriteLine(r_ip.ToString());
+
+                    users[id] = new user_t();
+                    var user = users[id];
+                    user.ip = r_ip;
+                }
+                packet_mgr.packet_read(byteBuffer.ToList(), id);
+            } catch (SocketException se) {
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+            }
         }
     }
 }
---
+++ YCCSNET/yc_packet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YCCSNET {
+
+    public static class packet_mgr{
+        public static Dictionary<EPacket_type, Func<List<byte>, int, List<byte>>> packet_recv_callback = new Dictionary<EPacket_type, Func<List<byte>, int, List<byte>>>();
+
+        public static List<byte> make_buffer(EPacket_type type, byte[] buf) {
+            var r = buf.ToList();
+            int size = r.Count;
+            r.InsertRange(0, BitConverter.GetBytes(size).ToList());
+            r.InsertRange(0, BitConverter.GetBytes((short)type).ToList());
+            return r;
+        }
+
+        public static void packet_read(List<byte> buf, int id) {
+            while (buf.Count != 0) {
+                short type = BitConverter.ToInt16(new byte[2] { buf[0], buf[1] });
+                buf.RemoveRange(0, 2);
+                buf = packet_recv_callback[(EPacket_type)type](buf, id);
+            }
+        }
+    }
+
+    class yc_packet {
+
+        [Serializable()]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        class p_input : packet_t<p_input> {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+            public string id;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+            public string passwd;
+        }
+
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public class packet_t<T> where T : class {
+            public packet_t() { }
+            public byte[] Serialize() {
+                var size = Marshal.SizeOf(typeof(T));
+                var array = new byte[size];
+                var ptr = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(this, ptr, true);
+                Marshal.Copy(ptr, array, 0, size);
+                Marshal.FreeHGlobal(ptr);
+                return array;
+            }
+            public static T Deserialize(byte[] array) {
+                var size = Marshal.SizeOf(typeof(T));
+                var ptr = Marshal.AllocHGlobal(size);
+                Marshal.Copy(array, 0, ptr, size);
+                var s = (T)Marshal.PtrToStructure(ptr, typeof(T));
+                Marshal.FreeHGlobal(ptr);
+                return s;
+            }
+        }
+    }
+}
